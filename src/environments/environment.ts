4107d9b5c343c4feb79c6669acd4fea4ae8abb92
pengw00 <water685@hotmail.com>
2019-05-16 09:43:33 -0400

    api did works

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://pengw00.github.io/angular-ngrx/src/assets/data/'
+  apiUrl:  'https://pengw00.github.io/angular-ngrx/src/assets/data/'
 };
 
 /*
